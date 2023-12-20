555de63d02d831ca575450aa07c93d55494a1200
Balazs Fodor <balazs.fodor@edu.bme.hu>
2023-12-20 11:24:38 +0100

    updates

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Challenge-ek
+
+## Challenge 1
+### Leírás
+A DevOps team az alábbira panaszkodik: telepítenek egy szolgáltatást (egy Service és egy Deployment), de hiába küldenek kérést a Service-nek, connection refused válasz jön.
+
+## Challenge 2
+
+## Challenge 3
+
+## Challenge 4
+
+## Challenge 5
+
+## Challenge 6
+
+## Challenge 7
+
+## Challenge 8
+
+## Challenge 9
+
+## Challenge 10
--- validate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kubectl run cpcv --restart=Never -i --leave-stdin-open --image=balazska/cloud-pulse-challenge-validator:0.0.6 --overrides="{\"spec\":{\"serviceAccountName\":\"$NEPTUN-sa\",\"containers\":[{\"image\":\"balazska/cloud-pulse-challenge-validator:0.0.6\",\"name\":\"cloud-pulse-challenge-validator\",\"command\": [\"./cloud-pulse-challenge-validator\",\"-c\",\"ch1\"],\"resources\":{\"limits\":{\"cpu\":\"100m\", \"memory\":\"64Mi\"}}}]}}" > /dev/null
-kubectl logs cpcv
-kubectl delete pod cpcv
